82dd57cf30be2820621296931129f08db1ffc54c
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-18 21:14:39 +0700

    Add project files.

---
+++ MathUnitTests2_7/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathUnitTests2_7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathUnitTests2_7")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85da2738-e287-4e41-8da0-8fea54b38429")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MathUnitTests2_7/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Black_box_Testing2_7;
+using NUnit.Framework;
+
+
+
+namespace MathUnitTests2_7
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+    }
+}
