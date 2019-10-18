e3d8eeb2d366d49ce1bc6fe0cabe428ea40d51e0
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-18 21:46:42 +0700

    2_7 modify

--- MathUnitTests2_7/UnitTest1.cs
+++ MathUnitTests2_7/UnitTest1.cs
@@ -14,8 +14,36 @@ namespace MathUnitTests2_7
             var math = new Math();
 
             var result = math.Add(1, 2);
+             
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
 
-            Assert.That(result, Is.EqualTo(3));
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_ArgumensAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
         }
 
     }
